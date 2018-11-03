796e64e38c0e1efaf1f0eaff8675bffb4cfe87d4
Mohammad Hematy <hematy61@gmail.com>
2018-11-02 20:40:54 -0500

    setting up boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const devMode = process.env.Node_ENV !== 'production';
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 9191,
+    open: 'google chrome'
+  },
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[local]--[hash:base64:5]',
+            sourceMap: true
+          }
+        }, 
+        {
+          loader: 'sass-loader'
+        }
+      ]
+    }
+  ],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+    })
+  ]
+};
